8081532261214fdb3b94a5f23a9db6226bba3f9b
Brian Ongati <mrbrianomollo@gmail.com>
2021-09-13 16:24:24 +0300

    add show hide jquery on what we do section

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,34 @@
 $(document).ready(function(){
+    $("#design").hide();
+    $("#development").hide();
+    $("#product-management").hide();
+    //design 
+    $("#design-image").click(function(){
+        $("#design-image").show().hide();
+        $("#design").show();
+      });
+      $("#design").click(function(){
+        $("#design").show().hide();
+        $("#design-image").show();
+      });
+    //development
+      $("#development-image").click(function(){
+        $("#development-image").show().hide();
+        $("#development").show();
+      });
+      $("#development").click(function(){
+        $("#development").show().hide();
+        $("#development-image").show();
+      });
+      //product-management
+      $("#product-management-image").click(function(){
+        $("#product-management-image").show().hide();
+        $("#product-management").show();
+      });
+      $("#product-management").click(function(){
+        $("#product-management").show().hide();
+        $("#product-management-image").show();
+      });
 //insert js code here
 
 
