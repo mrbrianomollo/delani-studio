415ba3cc57eabaaf3c9c4d084502c54737ccf19b
Brian Ongati <mrbrianomollo@gmail.com>
2021-09-15 14:29:52 +0300

    fix javascript function

--- js/scripts.js
+++ js/scripts.js
@@ -31,8 +31,8 @@ $(document).ready(function(){
       });
 });
 $(document).ready(function(){
-    $("form#form34A").submit(function(event){
-       event.preventDefault();
+    $("form#contact_us").submit(function(event){
+       //event.preventDefault();
       var name = $("input#MERGE1").val();
       var email = $("input#MERGE0").val();
       var message = $("textarea#comment").val();
