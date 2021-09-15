d4f2e164ca37bcdf65371748feb3bd0a3534c962
Brian Ongati <mrbrianomollo@gmail.com>
2021-09-15 13:06:43 +0300

    add footer

--- js/scripts.js
+++ js/scripts.js
@@ -29,29 +29,20 @@ $(document).ready(function(){
         $("#product-management").show().hide();
         $("#product-management-image").show();
       });
-//insert js code here
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 });
+$(document).ready(function(){
+    $("form#form34A").submit(function(event){
+       event.preventDefault();
+      var name = $("input#MERGE1").val();
+      var email = $("input#MERGE0").val();
+      var message = $("textarea#comment").val();
+      if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+        alert (name + ", we have received your message. Thank you for reaching out to us.");
+      }
+      else {
+        alert("Please enter your name and email!");
+      }
+      
+    });
+  
+  });
